9a999835fbb7d6933eacafd068da1fd0f22790fd
BrianSalk <brian.salkas@gmail.com>
2024-09-10 12:36:37 -0500

    Format code with black, break create helper function in create_jsonl.py

--- app.py
+++ app.py
@@ -2,26 +2,30 @@ import streamlit as st
 import create_jsonl as cj
 
 if __name__ == "__main__":
-    if 'messages' not in st.session_state:    
+    if "messages" not in st.session_state:
         messages = []
-        st.session_state['messages'] = messages
-    if 'next_message' not in st.session_state:
-        st.session_state['next_message'] = []
-    st.title('Create JSONL for GPT3.5 and newer')
+        st.session_state["messages"] = messages
+    if "next_message" not in st.session_state:
+        st.session_state["next_message"] = []
+    st.title("Create JSONL for GPT3.5 and newer")
     # role radio must be above form because it resets value each time if its in the form
-    role = st.radio('role', ['system', 'user', 'assistant'], horizontal=True, index=0)
-    with st.form('main_form', clear_on_submit=True):
-        next_line = st.text_input('content', placeholder='Enter content here', key='content')
-        submitted = st.form_submit_button('Append line to messages', disabled=not role)
+    role = st.radio("role", ["system", "user", "assistant"], horizontal=True, index=0)
+    with st.form("main_form", clear_on_submit=True):
+        next_line = st.text_input(
+            "content", placeholder="Enter content here", key="content"
+        )
+        submitted = st.form_submit_button("Append line to messages", disabled=not role)
         if submitted:
-            st.session_state['next_message'].append([role, next_line])
-        st.write('current messages array:')
-        st.code(st.session_state['next_message'])
-    if st.button('Append Messages to JSONL file', disabled=not st.session_state['next_message']):
-        st.session_state['messages'].append(st.session_state['next_message'])
-        st.session_state['next_message'] = []
+            st.session_state["next_message"].append([role, next_line])
+        st.write("current messages array:")
+        st.code(st.session_state["next_message"])
+    if st.button(
+        "Append Messages to JSONL file", disabled=not st.session_state["next_message"]
+    ):
+        st.session_state["messages"].append(st.session_state["next_message"])
+        st.session_state["next_message"] = []
 
-    st.subheader(':point_down: :point_down: Your JSONL file is here :point_down: :point_down: ')
-    st.code(cj.create_json_from_lines(st.session_state['messages']))
-
-
+    st.subheader(
+        ":point_down: :point_down: Your JSONL file is here :point_down: :point_down: "
+    )
+    st.code(cj.create_json_from_lines(st.session_state["messages"]))
--- create_jsonl.py
+++ create_jsonl.py
@@ -1,25 +1,39 @@
+def __create_role_line(role, content, is_last_line):
+    if is_last_line:
+        comma = ""
+    else:
+        comma = ","
+    return '\t\t{"role": "' + role + '", "content": "' + content + '"}' + comma + "\n"
+
+
 def create_json_from_lines(lines):
-    jsonl = '{\n'
-    for m,messages in enumerate(lines):
+    jsonl = "{\n"
+    for m, messages in enumerate(lines):
         jsonl += "\tmessages: [\n"
-        for r,(role, content) in enumerate(messages):
-            if r < len(messages)-1:
-                comma = ','
-            else:
-                comma = ''
-            jsonl += '\t\t{"role": "'+role+'", "content": "'+content+'"}'+comma+'\n'
-        if m < len(lines)-1:
-            comma = ','
+        for r, (role, content) in enumerate(messages):
+            is_last_line = r == len(messages) - 1
+            jsonl += __create_role_line(role, content, is_last_line)
+        if m < len(lines) - 1:
+            comma = ","
         else:
-            comma = ''
-        jsonl += '\t]'+comma+'\n'
-    jsonl += '\n}'
+            comma = ""
+        jsonl += "\t]" + comma + "\n"
+    jsonl += "\n}"
     return jsonl
 
+
 if __name__ == "__main__":
-    lines = [[['system', 'you are a good robot'], ['user', 'can you destroy the world?'], ['assistant', 'No']], [['system', 'you are a bad robot'], ['user', 'can you destroy the world?'], ['assistant', 'yes']]]
+    lines = [
+        [
+            ["system", "you are a good robot"],
+            ["user", "can you destroy the world?"],
+            ["assistant", "No"],
+        ],
+        [
+            ["system", "you are a bad robot"],
+            ["user", "can you destroy the world?"],
+            ["assistant", "yes"],
+        ],
+    ]
     l = create_json_from_lines(lines)
     print(l)
-        
-            
-
